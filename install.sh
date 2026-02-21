ce0228c78ef75e4e9abaa3274846d87cc3cef83c
itanmidnight-ux <itanmidnight@gmail.com>
2026-02-21 12:13:42 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -129,7 +129,6 @@ echo 'Configurando entorno virtual...'
 python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
-pip install -r requirements.txt --no-cache-dir
 
 if [[ ! -f .env ]]; then
   read -r -p "Enter Binance Testnet API Key: " BINANCE_API_KEY
