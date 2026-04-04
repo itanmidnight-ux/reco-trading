8f789e65941395950ba75b97483a324665cb36a3
itanmidnight-ux <itanmidnight@gmail.com>
2026-04-03 23:05:12 -0500

    Stabilize dashboards, adaptive filters, and expand web settings accordion

--- web_site/__init__.py
+++ web_site/__init__.py
@@ -1,4 +1,4 @@
 # Web Dashboard Package
-from .dashboard_server import set_bot_instance, run_server, run_in_thread
+from .dashboard_server import set_bot_instance, set_bot_instance_getter, run_server, run_in_thread
 
-__all__ = ['set_bot_instance', 'run_server', 'run_in_thread']
+__all__ = ["set_bot_instance", "set_bot_instance_getter", "run_server", "run_in_thread"]
